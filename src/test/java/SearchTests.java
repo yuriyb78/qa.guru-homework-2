b93fab5ff140273c0b156c90b541a6946793ccfc
Yuriy Belyaev <byur78@mail.ru>
2023-12-21 09:34:38 +0300

    изменен build.gradle чтобы можно было запустить тест

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SearchTests {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        Configuration.pageLoadStrategy = "eager";
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
 }
